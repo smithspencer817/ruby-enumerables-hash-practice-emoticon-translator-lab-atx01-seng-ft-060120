005686210cc555883f1837d40099f0be506c3de6
Spencer Smith <smithspencer817@gmail.com>
2020-05-07 20:25:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   emoticons = load_library(file_path)
   emoticons.each do |key|
-    if key[:english] == emoticon
+    if emoticons[key][:english] == emoticon
       emoticons[key][:japanese]
       binding.pry
     else
