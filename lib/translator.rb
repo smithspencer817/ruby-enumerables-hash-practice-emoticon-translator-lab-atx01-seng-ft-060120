db935fc7c2ee8db60867c9a2673747d99f65e89a
Spencer Smith <smithspencer817@gmail.com>
2020-05-07 19:49:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,17 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+  result = {}
+  
+  emoticons.each do |key, value|
+    result[key] = {}
+    result[key][:english] = value[0]
+    result[key][:japanese] = value[1]
+  end
+  result
 end
 
 def get_japanese_emoticon
