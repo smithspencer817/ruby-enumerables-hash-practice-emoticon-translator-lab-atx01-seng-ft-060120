84c7ae08f78c076185d90b581db3ddd422b03a64
Spencer Smith <smithspencer817@gmail.com>
2020-05-07 20:11:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  emoticons = YAML.load_file(file_path)
+  emoticons = load_library(file_path)
 end
 
 def get_english_meaning
